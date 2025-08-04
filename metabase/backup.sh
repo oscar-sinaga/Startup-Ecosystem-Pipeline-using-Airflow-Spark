95fc394149341432dec0de42c83152a06e3a44a5
oscar-sinaga <oscarmsinaga@gmail.com>
2025-08-04 23:43:55 +0700

    backup metabase

--- metabase/backup.sh
+++ metabase/backup.sh
@@ -1,1 +1,6 @@
-docker exec -t metabase-db pg_dump -U metabase_user -F p metabase_db > data/metabase_backup.sql
+#!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+docker exec -t metabase-db \
+  pg_dump -U metabase_user -F p metabase_db \
+  > "$SCRIPT_DIR/data/metabase_backup.sql"
